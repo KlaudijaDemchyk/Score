4d30e829d70cae9edf8a11b585b91770defb5b23
KlaudijaDemchyk <fkst@i.ua>
2023-06-18 15:29:53 +0200

    Test

--- score/src/App.test.js
+++ score/src/App.test.js
@@ -1,8 +1,29 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('renders app title', () => {
+    render(<App />);
+    const titleElement = screen.getByText('Рейтинг студентів');
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  test('updates student score', () => {
+    render(<App />);
+
+    const inputs = screen.getAllByTestId('score-input');
+    const input = inputs[0]; 
+    fireEvent.change(input, { target: { value: '80' } });
+    fireEvent.keyPress(input, { key: 'Enter', code: 13, charCode: 13 });
+
+    expect(input.value).toBe('80');
+  });
+
+  test('displays average score', () => {
+    render(<App />);
+
+    const averageScore = screen.getByText(/Середній бал:/);
+    expect(averageScore).toBeInTheDocument();
+  });
 });
--- score/src/components/ScoreInput.js
+++ score/src/components/ScoreInput.js
@@ -3,6 +3,7 @@ import React from 'react';
 const ScoreInput = ({ score, onScoreChange }) => (
   <td>
     <input 
+     data-testid="score-input"
       type="text" 
       value={score} 
       onChange={e => onScoreChange(Number(e.target.value))}
